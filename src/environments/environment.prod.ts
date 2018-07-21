539459046bed268e144068cd2b87ce92a7e9a9b8
jeanno <jeannory.phou@gmail.com>
2018-07-22 00:49:23 +0200

    method select menu product v2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,4 @@
 export const environment = {
   production: true,
-  firebase:  {
-    apiKey: "AIzaSyDafrguVhpPxL5jI4ktC_t4p6kmJpoqw_o",
-    authDomain: "terreiyaki-aae8f.firebaseapp.com",
-    databaseURL: "https://terreiyaki-aae8f.firebaseio.com",
-    projectId: "terreiyaki-aae8f",
-    storageBucket: "terreiyaki-aae8f.appspot.com",
-    messagingSenderId: "99059943066"
-}
+
 };
